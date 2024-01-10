78af8435cc696b27b59191cd6bd9c12aac7f1059
Alexis Ducastel <alexis@infrabuilder.com>
2024-01-10 22:14:54 +0100

    Add validated cilium setup

---
+++ setup/cni-configs/cilium.sh
@@ -0,0 +1,9 @@
+# Source : https://docs.cilium.io/en/stable/installation/k8s-install-helm/
+
+# helm show values --repo https://helm.cilium.io/ cilium 
+
+helm install cilium \
+    --namespace kube-system \
+    --repo https://helm.cilium.io/ \
+    --version 1.14.5 \
+    cilium 
