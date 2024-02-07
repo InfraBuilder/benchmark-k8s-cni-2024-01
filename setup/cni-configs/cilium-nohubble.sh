9469003594861566f359f4e814608a2e6729160e
Alexis Ducastel <alexis@infrabuilder.com>
2024-02-07 17:23:06 +0100

    add setup cilium nohubble

---
+++ setup/cni-configs/cilium-nohubble.sh
@@ -0,0 +1,10 @@
+# Source : https://docs.cilium.io/en/stable/installation/k8s-install-helm/
+
+# helm show values --repo https://helm.cilium.io/ cilium 
+
+helm install cilium \
+    --namespace kube-system \
+    --repo https://helm.cilium.io/ \
+    --version 1.14.6 \
+    cilium \
+    --set hubble.enabled=false
